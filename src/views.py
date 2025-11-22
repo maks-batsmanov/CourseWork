ffd50d671e65394063a95bc0370644aba8fd4e1e
Максим Бацманов <markgusev995@gmail.com>
2025-11-22 22:26:36 +0300

    update module views

--- src/views.py
+++ src/views.py
@@ -1,19 +1,10 @@
 import json
 import logging
 import os
-from src.utils import (
-    get_date_interval,
-    get_greeting,
-    read_excel,
-    selection_by_date,
-    executed_operations,
-    get_amount_by_card,
-    get_dict_with_cards,
-    get_top_transaction,
-    get_exchange_rate,
-    get_stocks,
-    reading_file_user_settings
-)
+
+from src.utils import (executed_operations, get_amount_by_card, get_date_interval, get_dict_with_cards,
+                       get_exchange_rate, get_greeting, get_stocks, get_top_transaction, read_excel,
+                       reading_file_user_settings, selection_by_date)
 
 current_dir = os.path.dirname(__file__)
 project_root = os.path.dirname(current_dir)
@@ -30,20 +21,20 @@ logger.setLevel(logging.DEBUG)
 def main_func(date_user):
     """Функцию принимает на вход строку с датой и временем в формате
     YYYY-MM-DD HH:MM:SS и возвращает JSON-ответ"""
-    logger.info('Функция main_func начала работу')
+    logger.info("Функция main_func начала работу")
     if not date_user or not isinstance(date_user, str):
-        logger.error('Функция main_func, некорректный формат даты')
+        logger.error("Функция main_func, некорректный формат даты")
         raise ValueError("Некорректный формат даты")
 
     greeting = get_greeting()
-    data_frame = read_excel('operations.xlsx')
+    data_frame = read_excel("operations.xlsx")
     interval = get_date_interval(date_user)
     selection = selection_by_date(data_frame, interval)
     status = executed_operations(selection)
     get_amount = get_amount_by_card(status)
     get_dict = get_dict_with_cards(get_amount)
     top_transaction = get_top_transaction(status)
-    data_json = reading_file_user_settings('user_settings.json')
+    data_json = reading_file_user_settings("user_settings.json")
     exchange_rate = get_exchange_rate(data_json)
     stocks = get_stocks(data_json)
 
@@ -52,14 +43,7 @@ def main_func(date_user):
         "cards": get_dict,
         "top transaction": top_transaction,
         "currency_rates": exchange_rate,
-        "stock_prices": stocks
+        "stock_prices": stocks,
     }
-    logger.info('Функция main_func отработала успешно')
+    logger.info("Функция main_func отработала успешно")
     return json.dumps(data_json, indent=4, ensure_ascii=False)
-
-
-
-
-if __name__ == '__main__':
-    data = main_func('2021-12-02 23:50:00')
-    print(data)
