f90ae5cb1cbfa54e5e1b5f5ee955b3d006f29c03
Максим Бацманов <markgusev995@gmail.com>
2025-11-23 19:10:39 +0300

    update services

--- src/services.py
+++ src/services.py
@@ -1,7 +1,7 @@
-from typing import Any, Dict, List
+import json
 import logging
 import os
-
+from typing import Any, Dict, List
 
 current_dir = os.path.dirname(__file__)
 project_root = os.path.dirname(current_dir)
@@ -24,7 +24,7 @@ def investment_bank(month: str, transactions: List[Dict[str, Any]], limit: int) 
         limit — предел, до которого нужно округлять суммы операций."""
     logger.info('Начало работы функции investment_bank')
     if not transactions or limit <= 0:
-        logger.error('Пустой датафрейм или неверно указан лимит')
+        logger.error('Пустой список или неверно указан лимит')
         return 0.0
     list_transact = [x for x in transactions
                      if x.get('Дата операции', '')[:7] == month
@@ -39,3 +39,31 @@ def investment_bank(month: str, transactions: List[Dict[str, Any]], limit: int) 
         return 0.0
     logger.info('Функция investment_bank отработала успешно')
     return round(list_round, 2)
+
+
+def read_json(file_name):
+    """Функция читает json-файл из папки data, и возвращает данные.
+        Принимает на вход имя файла из папки data"""
+    logger.info("Функция read_excel начала работу")
+    current_dir = os.path.dirname(__file__)
+    project_root = os.path.dirname(current_dir)
+    path_to_file = os.path.join(project_root, "data", file_name)
+    try:
+        if not os.path.exists(path_to_file):
+            logger.error("Ошибка, файл не найден")
+            return 0.0
+        if os.path.getsize(path_to_file) == 0:
+            logger.error("Ошибка, файл пустой")
+            return 0.0
+        logger.info("Открываем и преобразуем файл в список словарей")
+        with open(path_to_file, "r", encoding="utf-8") as file:
+            content = json.load(file)
+        if not isinstance(content, list):
+            logger.error("Ошибка, файл не является списком")
+            return 0.0
+        logger.info("Работа завершена успешно")
+        return content
+    except Exception as ex:
+        logger.error(f"Ошибка чтения файла {ex}")
+        print(f"Ошибка чтения файла {ex}")
+        return 0.0
